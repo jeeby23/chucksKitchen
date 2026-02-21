6df6ba4bb26f5cc58cd3c773efae29ec9721cc07
mujeeb <shola.mujeeb@gmail.com>
2026-02-21 22:11:36 +0100

    Add layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import AuthLayout from '@/components/auth/AuthLayout'
+import AuthLayout from '@/components/layouts/AuthLayout'
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
